bf92b06de609039763ec85618b503b889b956347
Liam Dawson <liamrdawson@gmail.com>
2025-03-31 22:22:48 +0100

    fix conflicts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -484,11 +484,9 @@ else -- NOTE: IF NOT VSCODE
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>ob', function()
+        vim.keymap.set('n', '<leader>sb', function()
           telescope.extensions.file_browser.file_browser(file_browser_opts)
-        vim.keymap.set('n', '<leader>sf', function()
-          builtin.find_files { hidden = true }
-        end, { desc = '[S]earch [F]iles' })
+        end, { desc = '[S]earch [B]rowser at current directory level' })
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
