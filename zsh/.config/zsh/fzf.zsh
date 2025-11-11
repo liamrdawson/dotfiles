64f9c4d0010656b8f767c4b4f2f8a910f2a4d3e9
Liam Dawson <liamrdawson@gmail.com>
2025-11-11 22:27:41 +0000

    tidy

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -11,12 +11,18 @@ export FZF_DEFAULT_OPTS='
 
 # Fuzzy find a file.
 function f() {
-    fzf --preview 'bat --theme=base16 --color=always --style=numbers --line-range=:500 {}' --height 90% --border
+    local INITIAL_QUERY="${*:-}" # All positional parameters or empty stirng
+    fzf --query "$INITIAL_QUERY" \
+        --preview 'bat --theme=base16 --color=always --style=numbers --line-range=:500 {}' \
+        --height 90% --border
 }
 
 # Fuzzy find a file and open in neovim.
 function fv () {
-    fzf --preview 'bat --theme=base16 --color=always --style=numbers --line-range=:500 {}' --height 90% --border | xargs -r nvim
+    local INITIAL_QUERY="${*:-}"
+    fzf --query "$INITIAL_QUERY" \
+        --preview 'bat --theme=base16 --color=always --style=numbers --line-range=:500 {}' \
+        --height 90% --border | xargs -r nvim
 }
 
 # Ripgrep for a word and use fzf for fuzzy finding within results.
@@ -24,19 +30,17 @@ function fg () {
     # 1. Search for text in files using Ripgrep
     # 2. Interactively restart Ripgrep with reload action
     # 3. Open the file in Vim
-    RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-    INITIAL_QUERY="${*:-}"
+    local RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+    # Here we're using the ${parameter:-word} expansion to expand the $* special
+    # parameter (all positional parameters). So, "Give me what's in parameter
+    # or otherwise give me word (even if that's an empty string)".
+    local INITIAL_QUERY="${*:-}"
     fzf --ansi --disabled --query "$INITIAL_QUERY" \
-        --bind "start:reload:$RG_PREFIX {q}" \
+        --bind "start:reload:$RG_PREFIX '{q}'" \
         --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
         --delimiter : \
         --preview 'bat --theme=base16 --color=always --line-range=:500 {1}' \
         --height 90% \
         --preview-window 'right,50%,border-bottom,+{2}+3/3,~3' \
         --bind 'enter:become(nvim {1} +{2})'
-}
-
-# Ripgrep a word in a particular file/filetype
-function fif() {
-    
 }
